f6789c851682ef21117421c6b68b6f126b745084
Khairul Anam Mubin <39878277+Khairul-Anam-Mubin@users.noreply.github.com>
2020-01-01 13:02:25 +0600

    Update 1007 - Mathematically Hard.cpp

--- Number Theory/1007 - Mathematically Hard.cpp
+++ Number Theory/1007 - Mathematically Hard.cpp
@@ -1,1 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std ;
 
+#define mxN 5000000
+unsigned long long phi[mxN + 10] ;
+
+void PreCalPhi() {
+    phi[0] = 0 ;
+    phi[1] = 1 ;
+    for(int i = 2 ; i <= mxN ; i++) {
+        if(phi[i] == 0) {
+            phi[i] = i - 1 ;
+            for(int j = i + i ; j <= mxN ; j += i) {
+                if(phi[j] == 0)
+                    phi[j] = j ;
+                phi[j] -= (phi[j] / i) ;
+            }
+        }
+    }
+    for(int i = 1 ; i <= mxN ; i++)
+        phi[i] = (phi[i] * phi[i]) ;
+    for(int i = 1 ; i <= mxN ; i++)
+        phi[i] += phi[i - 1] ;
+}
+int main() {
+    PreCalPhi() ;
+    int tc , test = 0 ;
+    cin >> tc ;
+    while(tc--) {
+        int a , b ;
+        cin >> a >> b ;
+        if(a > b)
+            swap(a , b) ;
+        cout << "Case " << ++test << ": " ;
+        cout << phi[b] - phi[a - 1] << "\n" ;
+    }
+    return 0 ;
+}
